56002fe65b13b56aede28e88ae21756b1d95a4cb
soma <devSomaPro@gmail.com>
2019-03-01 21:40:26 +0900

    webフレームワークginで簡単な文字列操作と出力をやってみた！！！

--- main.go
+++ main.go
@@ -6,21 +6,30 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type human struct {
+	name, message string
+}
+
+func (human human) Said() string {
+	return human.name + "さんが" + human.message + "と申しております"
+}
+
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("template/*")
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "ようこそ！！",
+			"title": "ようこそ！！!",
 		})
 	})
 	router.POST("/post", func(c *gin.Context) {
 		name := c.PostForm("name")
-		message := c.PostForm("message")
+		messageVal := c.PostForm("message")
+		human := human{name, messageVal}
+		said := human.Said()
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title":   "ようこそ！！",
-			"name":    name,
-			"message": message,
+			"title": "ようこそ！！",
+			"msg":   said,
 		})
 	})
 	router.Run(":8080")
