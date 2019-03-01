b2058a0ea46d629dd4ea1e637e000d78fc42dceb
soma <devSomaPro@gmail.com>
2019-03-01 17:30:50 +0900

    postデータを出力してみるところまで

--- main.go
+++ main.go
@@ -9,9 +9,18 @@ import (
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("template/*")
-	router.GET("/index", func(c *gin.Context) {
+	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title": "ようこそ！！",
+		})
+	})
+	router.POST("/post", func(c *gin.Context) {
+		name := c.PostForm("name")
+		message := c.PostForm("message")
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title":   "ようこそ！！",
+			"name":    name,
+			"message": message,
 		})
 	})
 	router.Run(":8080")
